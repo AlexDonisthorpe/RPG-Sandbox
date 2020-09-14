de2455ac9e706c8f92b45cfe438862b8d6d0d310
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-09-14 22:44:56 +0100

    Setup a raycast & debug line to visualise where the mouse is clicking in 3d space

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
+    Ray lastRay;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0)){
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         GetComponent<NavMeshAgent>().SetDestination(target.position);
     }
 }
