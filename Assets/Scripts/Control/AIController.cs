d29e30cccd8c8ac08b30d9771917de4de9171f37
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-09-21 12:09:53 +0100

    Added suspicion behaviour to the AI for when it loses track of the player

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -3,29 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 
 namespace RPG.Control{
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
+
         GameObject player;
         Health health;
         Fighter fighter;
+        Mover mover;
+        Vector3 guardLocation;
+
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start() {
-            fighter = GetComponent<Fighter>();
             player = GameObject.FindWithTag("Player");
+            fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+
+            guardLocation = transform.position;
         }
 
         private void Update()
         {
             if (health.IsDead()) return;
-            if(InAttackRange() && fighter.CanAttack(player)){
-                fighter.Attack(player);
-            } else {
-                fighter.Cancel();
+            if(InAttackRange() && fighter.CanAttack(player))
+            {
+                AttackBehaviour();
+                timeSinceLastSawPlayer = 0;
             }
+            else if(timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
+            }
+            else
+            {
+                GuardBehaviour();
+            }
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardLocation);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InAttackRange()
