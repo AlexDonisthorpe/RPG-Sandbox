04766156b02f7737625e337ba3ccd0b97c7c7729
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-10-06 16:48:35 +0100

    Portals can now be linked using the destination identifier within the portal prefab, this sets the spawn location of the player next to the related & linked portal.

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -8,17 +8,30 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
-        private void OnTriggerEnter(Collider other) {
+        private void OnTriggerEnter(Collider other) 
+        {
             if(other.gameObject.tag == "Player")
             {
                 StartCoroutine(Transition());
             }
         }
 
-        private IEnumerator Transition(){
+        private IEnumerator Transition()
+        {
+            if(sceneToLoad < 0){
+                Debug.LogError("sceneToLoad is not set.");
+                yield break;
+            }
+            
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
@@ -40,6 +53,7 @@ namespace RPG.SceneManagement
             Portal[] portals = FindObjectsOfType<Portal>();
             foreach(Portal portal in portals){
                 if(portal == this) continue;
+                if(portal.destination != destination) continue;
                 return portal;
             }
             return null;
