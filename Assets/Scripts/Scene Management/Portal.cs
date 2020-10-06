8b88935a56bf3481f70f6bb6066f40106c9a2fa5
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-10-06 16:31:19 +0100

    Set the character position to update to a specified spawn point when they load into a scene, allowing for scene transitions between areas to work as expected

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,20 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other) {
-            if(other.gameObject.tag == "Player"){
-            LoadScene(sceneToLoad);
+            if(other.gameObject.tag == "Player")
+            {
+                StartCoroutine(Transition());
             }
         }
 
-        public void LoadScene(int buildIndex){
-            SceneManager.LoadScene(buildIndex);
+        private IEnumerator Transition(){
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            Portal[] portals = FindObjectsOfType<Portal>();
+            foreach(Portal portal in portals){
+                if(portal == this) continue;
+                return portal;
+            }
+            return null;
         }
     }   
 }
