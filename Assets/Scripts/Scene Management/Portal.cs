7f9a147a309f7b2f80c544802f80c723c94393e3
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-10-06 14:45:19 +0100

    Created a basic portal which will change the scene when the player triggers it

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other) {
+            if(other.gameObject.tag == "Player"){
+            LoadScene(sceneToLoad);
+            }
+        }
+
+        public void LoadScene(int buildIndex){
+            SceneManager.LoadScene(buildIndex);
+        }
+    }   
+}
