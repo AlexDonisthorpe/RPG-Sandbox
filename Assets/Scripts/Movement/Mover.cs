77510ffabb5ad887930eae2ce1957ebf9b4cf030
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-10-29 18:21:01 +0000

    Now storing rotation on save as well to ensure consistent positoning

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -51,13 +51,20 @@ namespace RPG.Movement{
 
         public object CaptureState()
         {
-            return new SerializableVector3(transform.position);
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["position"] = new SerializableVector3(transform.position);
+            data["rotation"] = new SerializableVector3(transform.eulerAngles);
+            return data;
         }
 
         public void RestoreState(object state)
         {
-            SerializableVector3 newPosition = (SerializableVector3)state;
+            Dictionary<string, object> data = (Dictionary<string, object>)state;
+            SerializableVector3 newPosition = (SerializableVector3)data["position"];
+            SerializableVector3 newRotation = (SerializableVector3)data["rotation"];
+
             GetComponent<NavMeshAgent>().Warp(newPosition.ToVector());
+            transform.eulerAngles = newRotation.ToVector();
             GetComponent<ActionScheduler>().CancelCurrentAction();
         }
     }
