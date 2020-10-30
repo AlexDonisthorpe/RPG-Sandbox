c963dacc1dcf6e88332dea7d6be8d3eea6e70f35
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-10-30 15:59:49 +0000

    Added a an arrow projectile for the bow to shoot

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Transform target;
+
+private void Update() {
+    if(target == null) return;
+
+    transform.LookAt(GetAimLocation());
+    transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+}
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsuleCollider = target.GetComponent<CapsuleCollider>();
+        if(targetCapsuleCollider == null)
+        {
+            return target.position;
+        }
+        return target.position + (Vector3.up * targetCapsuleCollider.height / 2);
+    }
+}
