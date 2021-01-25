86c4291620404b2e5bf24fa93f344585c6f5cae1
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-01-25 16:35:48 +0000

    Fixed a fader bug whereby the faders (out and in) would overlap and never resolve properly if a second fader function is triggered before the first coroutine finishes

--- Assets/Scripts/Scene Management/Fader.cs
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -7,6 +7,7 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        Coroutine currentActiveFade = null;
         
         private void Awake() 
         {
@@ -15,20 +16,31 @@ namespace RPG.SceneManagement
 
         public IEnumerator FadeOut(float time)
         {
-            while(canvasGroup.alpha < 1) 
+            return Fade(1, time);
+        }
+
+        private IEnumerator FadeRoutine(float target, float time) 
+        {
+            while(!Mathf.Approximately(canvasGroup.alpha, target)) 
             {
-                canvasGroup.alpha += Time.deltaTime / time;
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
                 yield return null;
             }
         }
 
+        public IEnumerator Fade(float target, float time)
+        {
+            if (currentActiveFade != null)
+            {
+                StopCoroutine(currentActiveFade);
+            }
+            currentActiveFade = StartCoroutine(FadeRoutine(target, time));
+            yield return currentActiveFade;
+        }
+
         public IEnumerator FadeIn(float time)
         {
-            while (canvasGroup.alpha > 0) 
-            {
-                canvasGroup.alpha -= Time.deltaTime / time;
-                yield return null;
-            }
+            return Fade(0, time);
         }
 
         public void FadeOutImmediate()
