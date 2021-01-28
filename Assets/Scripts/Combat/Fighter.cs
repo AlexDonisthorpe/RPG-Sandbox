2ece037b2df04239e8eb179c114156b1419f71ae
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-01-28 14:31:59 +0000

    Fixed a bug whereby the player couldn't attack an enemy unless there was a navmesh path available

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -41,7 +41,7 @@ namespace RPG.Combat
 
             if (target == null || target.IsDead()) return;
 
-            if (!GetIsInRange())
+            if (!GetIsInRange(target.transform))
             {
                 if (isInAttack)
                 {
@@ -77,9 +77,9 @@ namespace RPG.Combat
             GetComponent<Animator>().SetTrigger("attack");
         }
 
-        private bool GetIsInRange()
+        private bool GetIsInRange(Transform targetTransform)
         {
-            return Vector3.Distance(gameObject.transform.position, target.transform.position) <= currentWeaponConfig.GetRange();
+            return Vector3.Distance(gameObject.transform.position, targetTransform.position) <= currentWeaponConfig.GetRange();
         }
 
         private void StopAttack()
@@ -119,7 +119,8 @@ namespace RPG.Combat
         public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) return false;
-            if (!GetComponent<Mover>().CanMoveTo(combatTarget.transform.position)) return false;
+            if (!GetComponent<Mover>().CanMoveTo(combatTarget.transform.position) && 
+                !GetIsInRange(combatTarget.transform)) return false;
             Health targetToTest = combatTarget.GetComponent<Health>();
             return targetToTest != null && !targetToTest.IsDead();
         }
